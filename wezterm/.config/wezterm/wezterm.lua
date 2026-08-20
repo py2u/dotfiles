e82fadf9b1a10f27b470c546986de9cbee60d19c
py2u <mengjian.me@outlook.com>
2026-08-20 22:09:16 +0800

    Change font to CaskaydiaCove Nerd Font Mono

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
 config.font_size = 18.0
 config.use_ime = true
 config.window_background_opacity = 0.85
